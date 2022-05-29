30eab05fb3bc41c03243176875f44a76215e3eb4
gcbui <you@example.com>
2022-05-28 17:31:40 -0700

    I resolved all of the changes Bola asked me

--- main.py
+++ main.py
@@ -23,19 +23,18 @@ class Frame:
             self.image += background_symbol
       
 current_frame = Frame()
- 
+
 def draw(x,y,symbol=None,symbols=None):
     if symbols == None:
         i = int((height-y) * (width) + (x-1))
         current_frame.image = current_frame.image[:i] + symbol + current_frame.image[i + 1:]
     else:
         for symbol in symbols:
-            i = int((height-y-symbol[1]) * (width) + (x+symbol[0]-1))
+            i = int((height-y-symbol[1]-1) * (width) + (x+symbol[0]-2))
             current_frame.image = current_frame.image[:i] + symbols[symbol] + current_frame.image[i + 1:]
 
 def render_frame():
     print(current_frame.banner+current_frame.image)
-
 
 def game():
     character_hash_map = {tuple([2,1]):'|', tuple([8,1]):'|', tuple([3,2]):'_',tuple([4,2]):'_',tuple([5,2]):'|',tuple([6,2]):'_',tuple([7,2]):'_', tuple([1,3]):'|',tuple([5,3]):'|',tuple([9,3]):'|',tuple([2,4]):'_', tuple([3,4]): '_',tuple([4,4]):'_',tuple([5,4]):'|',tuple([6,4]):'_',tuple([7,4]):'_',tuple([8,4]):'_', tuple([5,5]):'>'                        
@@ -51,7 +50,6 @@ def game():
         draw(width/2,height/2,symbols=character_hash_map)
         render_frame()
        
-
 game()
 
 
